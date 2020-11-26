6d83706670336f98b87c980fa8ddf631c2734936
Salih Cantekin <salih.cantekin@etstur.com>
2020-11-26 21:40:28 +0300

    Add project files.

---
+++ Services/ResAPI/Reservation.API.Models/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Reservation.API.Models
+{
+    public class Class1
+    {
+    }
+}
---
+++ Services/ResAPI/Reservation.API.Services/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Reservation.API.Services
+{
+    public class Class1
+    {
+    }
+}
